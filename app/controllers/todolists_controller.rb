5fcd5552cfa3abed11a2c99a6b1d14dff608e960
taisei <taipei001031@gmail.com>
2020-07-06 12:41:21 +0900

    finish 3-9

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -31,6 +31,13 @@ class TodolistsController < ApplicationController
 
   end
 
+  def destroy
+
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to todolists_path
+  end
+
   private
   def list_params
     params.require(:list).permit(:title, :body, :image)
